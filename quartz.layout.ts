e3a99fe5094cdb650944719b9e02c5ceeb2802ad
Ven Gist <vengist@odyssy.io>
2026-03-16 12:57:22 -0700

    Initial styles of typography and spacing

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [Component.Chat()],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "The Open Machine": "https://theopenmachine.net",
     },
   }),
 }
